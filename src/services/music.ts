d9554c52f6d121f4e6e7d940addfcf89a95040e3
Frank Fenton <echelon.iiii@gmail.com>
2017-03-05 02:38:40 +1100

    add back music

--- src/services/music.ts
+++ src/services/music.ts
@@ -5,22 +5,30 @@ import Tone from 'tone';
 
 @Injectable()
 export class MusicService {
+
   constructor() {
-    const synth = new Tone.Synth().toMaster()
+    const TIME = ['-', '+']
     const piano = new Tone.PolySynth(4, Tone.Synth, {
-			"volume" : -8,
+			"volume" : -16,
+      "envelope" : {
+        attackCurve: "exponential",
+        releaseCurve: "sine",
+				attack : 0.5,
+				decay : 0.5,
+				sustain : 0.2
+			},
 			"oscillator" : {
-				"partials" : [1, 2, 1],
+        type: "sine4"
 			},
 			"portamento" : 0.05
 		}).toMaster()
-    const loop = new Tone.Loop((time: any) =>{
+    const loop = new Tone.Loop((time: any) => {
       //triggered every eighth note. 
-      console.log(time);
-    }, "8n")
-    loop.humanize = true
-    loop.probability = 0.8
-    loop.start(0)
+      piano.triggerAttackRelease(Tone.Frequency(_.random(46, 60), 'midi'), `4n ${_.sample(TIME)} ${_.random(0, 0.3)}`)
+    }, "4n", {
+      humanize: true,
+      probability: 0.8
+    }).start(0)
   }
   play(): void {
     Tone.Transport.start(0)
