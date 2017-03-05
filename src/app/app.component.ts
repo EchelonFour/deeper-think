5e938ac55b35122bf950a6883823e0ec84046e65
Frank Fenton <echelon.iiii@gmail.com>
2017-03-05 14:12:22 +1100

    add random fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,22 @@ import { SpeechService } from '../services/speech';
 })
 export class AppComponent implements OnDestroy {
   phrase$: Observable<string>;
+  font$: Observable<string>;
+  private FONTS = ['Baloo', 'Crimson Text', 'Gidugu', 'Griffy', 'Indie Flower', 'Raleway', 'Ranga', 'Roboto', 'Supermercado One'];
 
   constructor(private af: AngularFire, private music: MusicService, private speech: SpeechService) {
     this.phrase$ = this.af.database.object('/phrase' /** TODO **/)
       .map((ref) => ref.$value)
-      .switchMap((phrase) => this.speech.speak$(phrase).startWith(phrase));
+      .switchMap((phrase) => this.speech.speak$(phrase).startWith(phrase))
+    this.font$ = this.phrase$.map((phrase) => this.newFont(phrase))
 
     this.music.play();
   }
 
   ngOnDestroy(): void {
     this.music.stop();
+  }
+  newFont(phrase: string): string {
+    return `${_.sample(this.FONTS)}, sans-serif`;
   }
 }
